63c2638f36b6dfdf5febc2fe25e2a138dc4440ff
Prashant Dave <prashantdave@yahoo.com>
2020-07-03 21:05:19 -0400

    End of section 5

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -7,8 +7,7 @@ namespace Persistence
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) :base(options)
-        {
-            
+        {           
         }
         public DbSet<Value> Values { get; set; }
         public DbSet<Activity> Activities { get; set; }
@@ -17,7 +16,7 @@ namespace Persistence
             .HasData( 
                 new Value { Id = 1, Name = "Value 101"},
                 new Value { Id = 2, Name = "Value 102"},
-                new Value {Id=3, Name = "Value 103"}
+                new Value { Id = 3, Name = "Value 103" }
             );
         }
     }
